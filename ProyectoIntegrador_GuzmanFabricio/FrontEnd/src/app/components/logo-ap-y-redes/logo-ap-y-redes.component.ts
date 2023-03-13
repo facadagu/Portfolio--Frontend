53655d501d6d5895a6229494632ed4b4e330cb37
David <davidguzman020403@gmail.com>
2023-03-13 00:53:09 -0300

    Creacion de carpeta FrontEnd

---
+++ ProyectoIntegrador_GuzmanFabricio/FrontEnd/src/app/components/logo-ap-y-redes/logo-ap-y-redes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-ap-y-redes',
+  templateUrl: './logo-ap-y-redes.component.html',
+  styleUrls: ['./logo-ap-y-redes.component.css']
+})
+export class LogoAPComponent {
+
+}

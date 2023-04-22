c488c79fa9d88182b6385cd1c373cf99068bf34a
David <davidguzman020403@gmail.com>
2023-04-22 15:17:37 -0300

    Login
    Se empezo a realizar la parte del backend del proyecto

--- ProyectoIntegrador_GuzmanFabricio/FrontEnd/src/app/app.module.ts
+++ ProyectoIntegrador_GuzmanFabricio/FrontEnd/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -13,6 +13,10 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysSkillsComponent } from './components/hys-skills/hys-skills.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component'
+import { interceptorProvider } from './service/interceptor-service';
 
 @NgModule({
   declarations: [
@@ -26,13 +30,19 @@ import { FooterComponent } from './components/footer/footer.component';
     HysSkillsComponent,
     ProyectosComponent,
     FooterComponent,
+    HomeComponent,
+    LoginComponent,
     ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({})
+    FormsModule,
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    interceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- ProyectoIntegrador_GuzmanFabricio/FrontEnd/src/app/components/logo-ap-y-redes/logo-ap-y-redes.component.ts
+++ ProyectoIntegrador_GuzmanFabricio/FrontEnd/src/app/components/logo-ap-y-redes/logo-ap-y-redes.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-logo-ap-y-redes',
   templateUrl: './logo-ap-y-redes.component.html',
   styleUrls: ['./logo-ap-y-redes.component.css']
 })
-export class LogoAPComponent {
+export class LogoAPComponent implements OnInit{
+  isLogged = false;
+
+  constructor(private router: Router, private tokenService: TokenService){}
+  
+  ngOnInit(): void {
+    if(this.tokenService.getToken()){
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
+  }
+
+  onLogOut():void{
+    this.tokenService.logOut();
+    window.location.reload();
+  }
+
+
+
+
+  login(){
+    this.router.navigate(['/login'])
+  }
 
 }

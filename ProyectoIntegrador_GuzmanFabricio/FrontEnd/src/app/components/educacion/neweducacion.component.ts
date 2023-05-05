a4af7aee6fc39718b4df2d10f0906012c35a4c9f
David <davidguzman020403@gmail.com>
2023-05-04 21:51:20 -0300

    Componente educacion

---
+++ ProyectoIntegrador_GuzmanFabricio/FrontEnd/src/app/components/educacion/neweducacion.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Educacion } from 'src/app/model/educacion';
+import { EducacionService } from 'src/app/service/educacion.service';
+
+@Component({
+  selector: 'app-neweducacion',
+  templateUrl: './neweducacion.component.html',
+  styleUrls: ['./neweducacion.component.css']
+})
+export class NeweducacionComponent {
+  nombreE: string;
+  descripcionE: string;
+
+  constructor(private educacionS : EducacionService, private router: Router){}
+
+  onCreate():void{
+    const educacion = new Educacion(this.nombreE, this.descripcionE);
+    this.educacionS.save(educacion).subscribe(
+      data => {
+        alert("Educacion añadida correctamente");
+        this.router.navigate(['']);
+      },err => {
+        alert("Fallo");
+        this.router.navigate(['']);
+      }
+    )
+  }
+}
---
+++ ProyectoIntegrador_GuzmanFabricio/FrontEnd/src/app/components/experiencia/new-experiencia.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Experiencia } from 'src/app/model/experiencia';
+import { SExperienciaService } from 'src/app/service/s-experiencia.service';
+
+@Component({
+  selector: 'app-new-experiencia',
+  templateUrl: './new-experiencia.component.html',
+  styleUrls: ['./new-experiencia.component.css']
+})
+export class NewExperienciaComponent {
+  nombreE : string = '';
+  descripcionE : string = '';
+
+  constructor(private sExperiencia : SExperienciaService, private router: Router){}
+
+  onCreate():void{
+    const expe = new Experiencia(this.nombreE, this.descripcionE);
+    this.sExperiencia.save(expe).subscribe(data => {
+      alert("Experiencia añadida");
+      this.router.navigate(['']);
+    }, err => {
+      alert("Fallo");
+      this.router.navigate(['']);
+    }
+    )
+  }
+}

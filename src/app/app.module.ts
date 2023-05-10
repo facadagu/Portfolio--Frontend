e205f4a832549de56f4e049c17275046ff3655dc
David <davidguzman020403@gmail.com>
2023-05-10 00:34:49 -0300

    Complemento skill

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,8 @@ import { NewExperienciaComponent } from './components/experiencia/new-experienci
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 import { NeweducacionComponent } from './components/educacion/neweducacion.component';
 import { EditeducacionComponent } from './components/educacion/editeducacion.component';
+import { EditSkillComponent } from './components/hys-skills/edit-skill.component';
+import { NewSkillComponent } from './components/hys-skills/new-skill.component';
 
 @NgModule({
   declarations: [
@@ -40,6 +42,8 @@ import { EditeducacionComponent } from './components/educacion/editeducacion.com
     EditExperienciaComponent,
     NeweducacionComponent,
     EditeducacionComponent,
+    EditSkillComponent,
+    NewSkillComponent,
     ],
   imports: [
     BrowserModule,
--- src/app/service/educacion.service.ts
+++ src/app/service/educacion.service.ts
@@ -2,12 +2,13 @@ import { Observable } from 'rxjs';
 import { Educacion } from '../model/educacion';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EducacionService {
-  URL = 'https://backenddavid.onrender.com/educacion/';
+  URL = environment.URL + 'educacion/';
 
   constructor(private httpClient : HttpClient) { }
 
